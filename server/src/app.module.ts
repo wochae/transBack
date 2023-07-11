46dbbf085361a67ae02efaf8a0dfe06bc854d25e
wochae <officialwochae@gmail.com>
2023-07-11 20:47:31 +0900

    FEAT: no repository, using database providers #1

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,15 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
     ChatModule,
-    DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+    ConfigModule.forRoot(),
+  ],
+  controllers: [],
 })
 export class AppModule {}
