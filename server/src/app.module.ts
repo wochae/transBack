0c378290df2fe1ab2b25cd6355f57fa3127efa57
wochae <officialwochae@gmail.com>
2023-07-13 19:30:06 +0900

    [FEAT] : user table would be generated successful #5

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,12 @@ import { ChatModule } from './chat/chat.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ChatModule,
+    UsersModule,
   ],
   controllers: [],
 })
