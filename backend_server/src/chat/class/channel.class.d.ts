40c975c85ba59f57c8359463fee31428da56faa4
김재환 <woghks7859@gmail.com>
2023-07-23 17:24:25 +0900

    FEAT: Create Channel class define file

---
+++ backend_server/src/chat/class/channel.class.d.ts
@@ -0,0 +1,34 @@
+// FIXME: private 에 _ 붙이는 거 어떻게 생각하는지..?
+// FIXME: any type 을 적절하게 수정해야함
+declare class Channel {
+  /******************************* 멤버 변수 *******************************/
+  private _channelIdx: number;
+  private _roomId: number;
+  private _member: any; //   private member: member[];
+  private _message: any; //   private message: message[];
+  private _mode: string; // enum 으로 수정
+  private _owner: any; //   private owner: member;
+  private _admin: any; //   private admin: member[];
+  private _password: string;
+
+  /******************************* 메서드 *******************************/
+  // getter
+  get getChannelIdx(): number;
+  get getRoomId(): number;
+  get getMember(): any; //   member: member[];
+  get getMessage(): any; //   message: message[];
+  get getMode(): string; // enum 으로 수정
+  get getOwner(): any; //   owner: member;
+  get getAdmin(): any; //   admin: member[];
+  get getPassword(): string;
+
+  // setter
+  set setChannelIdx(channelIdx: number);
+  set setRoomId(roomId: number);
+  set setMember(member: any); // member: member[];
+  set setMessage(message: any); // message: message[];
+  set setMode(mode: string); // enum 으로 수정
+  set setOwner(owner: any); // owner: member;
+  set setAdmin(admin: any); // admin: member[];
+  set setPassword(password: string);
+}
