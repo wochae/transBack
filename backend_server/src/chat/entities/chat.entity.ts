0a757e7a1e3581b9407e0389a0c68037771c85cd
김재환 <woghks7859@gmail.com>
2023-07-23 19:02:44 +0900

    FEAT: Add Message & Channel & ChannelMember entity with typeORM

--- backend_server/src/chat/entities/chat.entity.ts
+++ backend_server/src/chat/entities/chat.entity.ts
@@ -1,1 +1,84 @@
-export class Chat {}
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Channel extends BaseEntity {
+  @PrimaryColumn()
+  channelIdx: number;
+
+  @Column()
+  channelName: string;
+
+  //   @Column({ nullable: true })
+  //   onwer: number;
+
+  // 근데 우리 비밀번호 컬럼 데이터에 저장할 때 비밀번호 암호화 처리해야함.
+  //   @Column({ nullable: true })
+  //   password: string;
+
+  @OneToMany(() => ChannelMember, (channelMember) => channelMember.channel)
+  channelMembers: ChannelMember[];
+}
+
+@Entity()
+export class ChannelMember extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
+  @Column()
+  userIdx: number;
+
+  //   @Column()
+  //   channelType: number;
+
+  @ManyToOne(() => Channel, (channel) => channel.channelMembers)
+  channel: Channel;
+}
+
+@Entity()
+export class Message extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
+  @Column()
+  channelIdx: number;
+
+  @Column()
+  sender: number;
+
+  @Column()
+  msg: string;
+
+  @Column()
+  msgDate: Date;
+
+  // date 타입 컬럼은 나중에 넣기
+}
+
+// joinedChannel, channel, message
+/*
+@Entity()
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+
+  @ManyToOne((type) => User, (user) => user.boards, { eager: false })
+  user: User;
+}
+*/
