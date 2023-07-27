fd8e7921e7f8cf65565926bb0e9477bbf72b24e5
김재환 <woghks7859@gmail.com>
2023-07-27 14:20:13 +0900

    CHORE: Remove comment

--- backend_server/src/chat/chat.module.ts
+++ backend_server/src/chat/chat.module.ts
@@ -4,6 +4,7 @@ import { ChatGateway } from './chat.gateway';
 import { Chat } from './class/chat.class';
 import { Channel } from './class/channel.class';
 import { Message } from './class/message.class';
+import { Mode } from './entities/chat.entity';
 
 @Module({
   // TODO: Member 와 관련된 것을 추가해야함
@@ -13,21 +14,21 @@ export class ChatModule {
   private logger: Logger = new Logger('ChatModule');
   constructor(private chat: Chat) {
     // FIXME: 테스트용 코드
-    // TODO: enum 추가
     const channel1 = new Channel();
     const testMsg = new Message(1, 1, 'test');
     testMsg.setMsgDate = new Date();
+
     channel1.setChannelIdx = 1;
     channel1.setRoomId = 1;
     this.logger.log('[ 💬 TEST ] Test Chat Object Init!');
-    channel1.setMode = 'public';
+    channel1.setMode = Mode.PUBLIC;
     channel1.setOwner = null;
     channel1.setMessage = testMsg;
     channel1.setMember = ['jaekim'];
     // Public Room TEST
-    // channel1.setPassword = null;
+    channel1.setPassword = null;
     // Protected Room TEST
-    channel1.setPassword = '1234';
+    // channel1.setPassword = '1234';
 
     this.chat.setProtectedChannels = channel1;
     console.log(this.chat.getProtectedChannels[0]);
