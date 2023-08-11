02007045f06b7c9db7d858732cdaa9c9d36ec502
wochae <officialwochae@gmail.com>
2023-08-11 20:21:03 +0900

    [WIP] : show user profile #90

--- backend_server/src/users/dto/insert-friend.dto.ts
+++ backend_server/src/users/dto/insert-friend.dto.ts
@@ -5,6 +5,17 @@ import {
   Matches,
   IsNotEmpty,
 } from 'class-validator';
+import { FriendList } from '../../entity/friendList.entity';
+
+export class InsertFriendReqDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @MinLength(1)
+  @Matches(/^[a-zA-Z0-9]*$/, { message: 'intra is unique' })
+  targetNickname: string;
+  targetIdx : number;
+}
 
 export class InsertFriendDto {
   @IsNotEmpty()
@@ -13,4 +24,15 @@ export class InsertFriendDto {
   @MinLength(1)
   @Matches(/^[a-zA-Z0-9]*$/, { message: 'intra is unique' })
   targetNickname: string;
+}
+
+
+export class InsertFriendResDto {
+  friendList: FriendList[]; // 이거 하나의 객체인데 왜 이름이 리스트 ㅋㅋ
+}
+
+export class FriendDto {
+  frindNickname : string;
+  friendIdx : number;
+  isOnline : boolean;
 }
--- backend_server/src/users/users.controller.ts
+++ backend_server/src/users/users.controller.ts
@@ -4,31 +4,41 @@ import {
   Post,
   Body,
   BadRequestException,
+  Redirect,
+  Get,
+  Res,
+  Query,
+  Logger,
+  UseGuards,
+  Headers,
+  HttpStatus,
+  Req,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { UsersService } from './users.service';
 import { CreateUsersDto } from './dto/create-users.dto';
+import { AuthService,} from 'src/auth/auth.service';
+import { plainToClass } from 'class-transformer';
+import { UserObject } from '../entity/users.entity';
 
-@Controller()
+
+@Controller("users")
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    ) {}
+    private logger: Logger = new Logger('UserController');
 
-  @Post('/auth')
-  signUp(
-    @Body(ValidationPipe) createUsersDto: CreateUsersDto,
-  ): Promise<string> {
-    const data = this.userService.signUp(createUsersDto);
-
-    if (data === null) {
-      throw new BadRequestException('this is not unique intra');
+    @Get("profile")
+    async getUserProfile(@Headers('authorization') authHeader: any, @Req() req, @Res() res: Response) {
+      try {
+        authHeader = authHeader.startsWith('Bearer') ? authHeader.split(' ')[1] : req.headers.authorization;
+        const user = await this.usersService.getTokenInfo(authHeader);
+        const userProfile = await this.usersService.findOneUser(user.userIdx);
+        return res.status(HttpStatus.OK).json(userProfile);
+      } catch (err) {
+        this.logger.error(err);
+        return res.status(HttpStatus.BAD_REQUEST).json({ message: err.message });
+      }
     }
-
-    return data;
-  }
-
-  @Post('/auth/login')
-  signIn(
-    @Body(ValidationPipe) createUsersDto: CreateUsersDto,
-  ): Promise<string> {
-    return this.userService.signIn(createUsersDto);
-  }
 }
