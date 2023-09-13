1548b70e58d2cacf4307e9c921d9c417bb0cf089
wochae <officialwochae@gmail.com>
2023-09-13 20:44:52 +0900

    [FIX] : disconnect user exit room when tab closed #192

--- backend_server/src/chat/chat.service.ts
+++ backend_server/src/chat/chat.service.ts
@@ -563,6 +563,7 @@ export class ChatService {
   /******************* Funcions about Exit Room *******************/
 
   goToLobby(client: Socket, channel: Channel, user: UserObject) {
+    console.log('chat Service : goToLobby');
     const isOwner: boolean = channel.getOwner.userIdx === user.userIdx;
     const isAdmin: boolean = channel.getAdmin.some(
       (member) => member.userIdx === user.userIdx,
@@ -584,8 +585,9 @@ export class ChatService {
         mode: channel.mode,
       };
     });
+    client.emit('chat_goto_lobby', channelsInfo);
     client.leave(`chat_room_${channel.getChannelIdx}`);
-    return channelsInfo;
+    
   }
 
   exitAnnounce(channel: Channel) {
