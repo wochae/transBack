38c3a7704ac78c2f90ebe86a48169df899ee6dfd
jae-hwan-kim <woghks7859@gmail.com>
2023-09-20 15:02:41 +0900

    FIX: Fix chat_goto_lobby

--- backend_server/src/chat/chat.gateway.ts
+++ backend_server/src/chat/chat.gateway.ts
@@ -853,19 +853,19 @@ export class ChatGateway
       );
     }
     // API: MAIN_CHAT_8
-    this.announceExit(channel);
-  }
-  async announceExit(channel: Channel) {
-    const announce = await this.chatService.exitAnnounce(channel);
-    console.log('announce : ', announce);
-    this.server
-      .to(`chat_room_${channel.getChannelIdx}`)
-      .emit('chat_room_exit', announce);
+  this.announceExit(channel);
     return this.messanger.setResponseMsgWithLogger(
       200,
       'Done exit room',
-      'chat_room_exit',
+      'chat_goto_lobby',
     );
+  }
+
+  async announceExit(channel: Channel) {
+    const announce = await this.chatService.exitAnnounce(channel);
+    this.server
+      .to(`chat_room_${channel.getChannelIdx}`)
+      .emit('chat_room_exit', announce);
   }
 
   // API: MAIN_CHAT_12
@@ -1249,14 +1249,15 @@ export class ChatGateway
     if (target.isOnline === OnlineStatus.ONGAME) {
       return new ReturnMsgDto(400, 'Bad Request, target user is on Game');
     } else if (target.isOnline === OnlineStatus.ONLINE) {
+      await this.usersService.setIsOnline(target, OnlineStatus.ONGAME);
       const invitaionCard = new GameInvitationAskDto(
         myObject.userIdx,
         myObject.nickname,
       );
-      console.log(invitaionCard);
-      console.log(target.userIdx);
-      console.log(target.nickname);
-      console.log(targetSocket.id);
+      // console.log(invitaionCard);
+      // console.log(target.userIdx);
+      // console.log(target.nickname);
+      // console.log(targetSocket.id);
       setTimeout(() => {
         targetSocket.emit('chat_invite_answer', invitaionCard);
       }, 100);
