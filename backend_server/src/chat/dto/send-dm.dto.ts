35c8d07085b6f6ba183f3c5d337c888e64246868
haryu <haryu@student.42seoul.kr>
2023-09-23 01:56:00 +0900

    FIX: add validation check for msg

--- backend_server/src/chat/dto/send-dm.dto.ts
+++ backend_server/src/chat/dto/send-dm.dto.ts
@@ -7,13 +7,13 @@ import {
 } from 'class-validator';
 
 export class SendDMDto {
-  // @IsNotEmpty()
-  // @IsString()
   // @MaxLength(100)
   // @MinLength(1)
-  // @Matches(/^[a-zA-Z0-9]*$/, { message: 'nickname is unique' })
   // targetNickname: string;
 
+  @IsString()
+  @IsNotEmpty()
+  //   @Matches(/^[a-zA-Z0-9]*$/, { message: 'nickname is unique' })
   @MaxLength(2000)
   msg: string;
 }
